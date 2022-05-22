e77a3746511f2e7e540e43574d301230b09e06c0
Noman Sarfraz <bsef19m503@pucit.edu.pk>
2022-05-23 02:50:20 +0500

    Added dynamicity in app

--- 6-Kids_App/Mobile_Computing/app/src/main/java/com/noman/mobile_computing/LessonActivity.java
+++ 6-Kids_App/Mobile_Computing/app/src/main/java/com/noman/mobile_computing/LessonActivity.java
@@ -26,16 +26,18 @@ public class LessonActivity extends AppCompatActivity {
         textViews = new int[]{ R.id.textView1, R.id.textView2, R.id.textView3, R.id.textView4 };
 
 
-        images = new int[26][4];
-        names = new String[26][4];
-        images[0][0] = R.drawable.a1;
-        images[0][1] = R.drawable.a2;
-        images[0][2] = R.drawable.a3;
-        images[0][3] = R.drawable.a4;
-        names[0][0] = "Apple";
-        names[0][1] = "Airplane";
-        names[0][2] = "Alligator";
-        names[0][3] = "Aunt-Ant";
+        images = new int[][] {
+            {R.drawable.a1, R.drawable.a2, R.drawable.a3, R.drawable.a4},
+            {R.drawable.b1, R.drawable.b2, R.drawable.b3, R.drawable.b4},
+            {R.drawable.c1, R.drawable.c2, R.drawable.c3, R.drawable.c4},
+            {R.drawable.d1, R.drawable.d2, R.drawable.d3, R.drawable.d4}
+        };
+        names = new String[][] {
+                {"Apple", "Airplane", "Alligator", "Aunt-Man"},
+                {"Bat", "Ball", "Balloon", "Bear"},
+                {"Cat", "Cake", "Car", "Crow"},
+                {"Duck", "Dog", "Dear", "Dinosaur"}
+        };
 
         if(getIntent() != null && getIntent().hasExtra("alphabet")) {
             index = getIntent().getStringExtra("alphabet").charAt(0) - 65;
--- 6-Kids_App/Mobile_Computing/app/src/main/java/com/noman/mobile_computing/QuizActivity.java
+++ 6-Kids_App/Mobile_Computing/app/src/main/java/com/noman/mobile_computing/QuizActivity.java
@@ -2,40 +2,50 @@ package com.noman.mobile_computing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
     private ImageView[] imageViews;
     private ImageView image;
+    private TextView scoreBox;
+    private TextView[] questions;
+    private String[] questionsTexts;
     private RadioButton selectedButton;
+    private RadioButton rightButton;
     private RadioButton radioButton;
     private RadioGroup[] radioGroups;
     private int[] images;
-    private String[][] options;
+    private String[][] optionsTexts;
+    private RadioButton[][] options;
     private int[] answers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        Initialization();
+        setQuiz();
     }
 
-    public void gradeQuiz(View view) {
-
-    }
 
     private void setQuiz() {
-        for(int i=0;i<5;i++) {
+        for (int i = 0; i < 5; i++) {
+            questions[i].setText(questionsTexts[i]);
             imageViews[i].setImageResource(images[i]);
+            for (int j = 0; j < 4; j++) {
+                options[i][j].setText(optionsTexts[i][j]);
+            }
         }
-
-       image = findViewById(R.id.image1);
     }
 
     private void Initialization() {
@@ -45,21 +55,41 @@ public class QuizActivity extends AppCompatActivity {
                  findViewById(R.id.image3),
                  findViewById(R.id.image4),
                  findViewById(R.id.image5)
-         }
+         };
          images = new int[]{ R.drawable.c1, R.drawable.a4, R.drawable.b2, R.drawable.c4, R.drawable.a1 };
+         questions = new TextView[] {
+                 findViewById(R.id.question1),
+                 findViewById(R.id.question2),
+                 findViewById(R.id.question3),
+                 findViewById(R.id.question4),
+                 findViewById(R.id.question5)
+         };
+         questionsTexts = new String[] {
+                 "Q1: The name of the following object starts with:",
+                 "Q2: The name of the following object starts with:",
+                 "Q3: The name of the following object starts with:",
+                 "Q4: The name of the following object starts with:",
+                 "Q5: The name of the following object starts with:"
+         };
+         options = new RadioButton[][] {
+                 {findViewById(R.id.q1_option1), findViewById(R.id.q1_option2), findViewById(R.id.q1_option3), findViewById(R.id.q1_option4)},
+                 {findViewById(R.id.q2_option1), findViewById(R.id.q2_option2), findViewById(R.id.q2_option3), findViewById(R.id.q2_option4)},
+                 {findViewById(R.id.q3_option1), findViewById(R.id.q3_option2), findViewById(R.id.q3_option3), findViewById(R.id.q3_option4)},
+                 {findViewById(R.id.q4_option1), findViewById(R.id.q4_option2), findViewById(R.id.q4_option3), findViewById(R.id.q4_option4)},
+                 {findViewById(R.id.q5_option1), findViewById(R.id.q5_option2), findViewById(R.id.q5_option3), findViewById(R.id.q5_option4)},
+         };
          optionsTexts = new String[][] {
                  {"D","B","C","A"},
                  {"D","B","C","A"},
                  {"D","B","C","A"},
                  {"D","B","C","A"},
-                 {"A","B","C","D"}
-         };
+                 {"D","B","C","A"}         };
          answers = new int[] {
                  R.id.q1_option3,
-                 R.id.q2_option1,
+                 R.id.q2_option4,
                  R.id.q3_option2,
                  R.id.q4_option3,
-                 R.id.q5_option1
+                 R.id.q5_option4
          };
          radioGroups = new RadioGroup[] {
                  findViewById(R.id.answer1),
@@ -69,5 +99,29 @@ public class QuizActivity extends AppCompatActivity {
                  findViewById(R.id.answer5)
          };
         }
-    }
+        @SuppressLint("ResourceAsColor")
+        public void gradeQuiz(View view) {
+            int score = 0;
+            for (int i = 0; i < 5; i++) {
+                if(radioGroups[i].getCheckedRadioButtonId() == answers[i]) {
+                    score++;
+                }
+//                if(radioGroups[i].getCheckedRadioButtonId() == -1) {
+//                    radioButton = findViewById(answers[i]);
+//                    radioButton.setBackgroundColor(R.color.purple_700);
+//                } else if(radioGroups[i].getCheckedRadioButtonId() == answers[i]) {
+//                    radioButton = findViewById(answers[i]);
+//                    radioButton.setBackgroundColor(R.color.red);
+//                } else if(radioGroups[i].getCheckedRadioButtonId() != answers[i]) {
+//                    radioButton = findViewById(answers[i]);
+//                    radioButton.setBackgroundColor(R.color.red);
+//                    radioButton = findViewById(radioGroups[i].getCheckedRadioButtonId());
+//                    radioButton.setBackgroundColor(R.color.red);
+//                }
+            }
+            scoreBox = findViewById(R.id.scoreBox);
+            scoreBox.setText("Your final score: " + Integer.toString(score));
+            Toast.makeText(this, "Your score: " + Integer.toString(score), Toast.LENGTH_SHORT).show();
+            }
+
 }
