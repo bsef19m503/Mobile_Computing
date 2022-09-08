cb9d529664392d173851d9287152df29f9def742
Noman Sarfraz <bsef19m503@pucit.edu.pk>
2022-09-08 06:28:56 +0500

    added Update and Delete funtionalities | Database_CRUD

--- 7-Database_CRUD/DB_CRUD/app/src/main/java/com/noman/dialogues/MainActivity.java
+++ 7-Database_CRUD/DB_CRUD/app/src/main/java/com/noman/dialogues/MainActivity.java
@@ -16,16 +16,13 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-    DatabaseHelper databaseHelper;
-
     ArrayList<String> studentNames = new ArrayList<String>(
             Arrays.asList("Umar", "Ali", "Usman"));
     ArrayList<String> studentIDs = new ArrayList<String>(
             Arrays.asList("1", "2", "3"));
-    ArrayList<StudentModel> studentsList;
+
     ListView listView;
 
     Button addBtn;
@@ -37,12 +34,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        databaseHelper = new DatabaseHelper(MainActivity.this);
-        studentsList = databaseHelper.getALLStudents();
 
         listView = findViewById(R.id.recordView);
 //      ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_listview, R.id.dayView, daysList);
-        SingleRecordAdapter singleRecordAdapter = new SingleRecordAdapter(getApplicationContext(), MainActivity.this, studentNames, studentIDs, studentsList);
+        SingleRecordAdapter singleRecordAdapter = new SingleRecordAdapter(getApplicationContext(), MainActivity.this, studentNames, studentIDs);
 //        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 //            @Override
 //            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -56,14 +51,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 idInput = findViewById(R.id.idInput);
                 nameInput = findViewById(R.id.nameInput);
-                Date date = new Date();
                 studentIDs.add((idInput.getText().toString()));
                 studentNames.add((nameInput.getText().toString()));
-                StudentModel studentModel = new StudentModel("", idInput.getText().toString(), nameInput.getText().toString(), "" + date.getTime());
-                Toast.makeText(MainActivity.this, studentModel.getReg_no()+" | "+studentModel.getName()+" | "+studentModel.getCreated_at(), Toast.LENGTH_SHORT).show();
-                databaseHelper.addStudent(studentModel);
-
-                singleRecordAdapter.refresh();
+                singleRecordAdapter.notifyDataSetChanged();
                 idInput.setText("");
                 nameInput.setText("");
             }
@@ -74,8 +64,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 listView.setAdapter(singleRecordAdapter);
+
             }
         });
+
+
+
 
     }
 }
